5c84cfb7ee8742c0de6ca965eebf0cc51270e52d
simpson <450054@naver.com>
2020-12-28 12:29:17 +0900

    201228_Start

---
+++ app/src/main/java/simp/test/andtoird_workspace/MainActivity.java
@@ -0,0 +1,14 @@
+package simp.test.andtoird_workspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
